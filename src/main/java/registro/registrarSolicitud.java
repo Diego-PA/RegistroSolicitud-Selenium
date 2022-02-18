dc575449350e2eef1f81b357592173a4b91c452c
DiegoPA <diego.loge@hotmail.com>
2022-02-18 16:03:55 -0600

    Initial commit

---
+++ src/main/java/registro/registrarSolicitud.java
@@ -0,0 +1,40 @@
+package registro;
+
+import static java.lang.System.setProperty;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class registrarSolicitud {
+
+    public static void main(String[] args) {
+
+        //Para ms edge
+        //Elegimos el driver
+        setProperty("webdriver.edge.driver", "C:\\Users\\diego\\IdeaProjects\\SeleniumTestProject\\src\\Drivers\\msedgedriver.exe");
+
+        //Inicializa el navegador
+        WebDriver driver = new EdgeDriver();
+
+        //Le damos la propiedad de ejecutar scripts de JS
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        driver.manage().window().maximize();
+
+        //Abrimos la página web
+        //8000
+        driver.get("http://127.0.0.1:80/");
+
+        driver.findElement(By.id("cuenta")).sendKeys("314034957");
+
+        driver.findElement(By.id("nip")).sendKeys("1234");
+
+        driver.findElement(By.xpath("//*[@id=\"form-alumno\"]/div/button")).click();
+    }
+
+}
